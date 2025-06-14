3c08c784845e1c36ea09c29de12779e70d25dfab
Ilya <i.sevrin@yandex.ru>
2025-06-14 14:52:13 +0300

    Style fix

--- src/test/java/ru/yandex/practicum/filmorate/UserControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/UserControllerTest.java
@@ -18,8 +18,7 @@ class UserControllerTest {
     @BeforeEach
     void setUp() {
         userController = new UserController();
-        validUser = new User(0L, "valid@example.com", "valid_login", "Valid User",
-                LocalDate.of(2000, 1, 1));
+        validUser = new User(0L, "valid@example.com", "valid_login", "Valid User", LocalDate.of(2000, 1, 1));
     }
 
     @Test
@@ -30,30 +29,20 @@ class UserControllerTest {
         assertEquals("valid@example.com", createdUser.getEmail(), "Email не совпадает");
         assertEquals("valid_login", createdUser.getLogin(), "Логин не совпадает");
         assertEquals("Valid User", createdUser.getName(), "Имя не совпадает");
-        assertEquals(LocalDate.of(2000, 1, 1), createdUser.getBirthday(),
-                "Дата рождения не совпадает");
+        assertEquals(LocalDate.of(2000, 1, 1), createdUser.getBirthday(), "Дата рождения не совпадает");
     }
 
     @Test
     void create_WithEmptyUser_ShouldThrowValidationException() {
         User emptyUser = new User(0L, "", "", "", LocalDate.now());
-        ValidationException exception = assertThrows(
-                ValidationException.class,
-                () -> userController.create(emptyUser),
-                "Ожидалось ValidationException при пустом пользователе!"
-        );
+        ValidationException exception = assertThrows(ValidationException.class, () -> userController.create(emptyUser), "Ожидалось ValidationException при пустом пользователе!");
         assertTrue(userController.getUsers().isEmpty(), "Пустой пользователь не должен добавляться");
     }
 
     @Test
     void create_WithInvalidUserShouldThrowValidationException() {
-        User emptyUser = new User(0L, "asddas", "dS sas", "",
-                LocalDate.of(2040, 1, 1));
-        ValidationException exception = assertThrows(
-                ValidationException.class,
-                () -> userController.create(emptyUser),
-                "Ожидалось ValidationException при неверных данных"
-        );
+        User emptyUser = new User(0L, "asddas", "dS sas", "", LocalDate.of(2040, 1, 1));
+        ValidationException exception = assertThrows(ValidationException.class, () -> userController.create(emptyUser), "Ожидалось ValidationException при неверных данных");
         assertTrue(userController.getUsers().isEmpty(), "Пользователь с неверными данными не должен добавляться");
     }
 
